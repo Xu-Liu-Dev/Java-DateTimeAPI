330ff97b987b88de54f477dc0e9c0b5ebb1c8b6d
liuxu.dev <liuxu.dev.git@gmail.com>
2025-12-29 12:35:07 +0900

    init common

--- DateTimeAPI/src/main/java/de/datetime/api/basic/ConvertDateToString.java
+++ DateTimeAPI/src/main/java/de/datetime/api/basic/ConvertDateToString.java
@@ -1,4 +1,4 @@
-package de.datetime.api;
+package de.datetime.api.basic;
 
 import java.text.SimpleDateFormat;
 import java.util.Date;
@@ -9,6 +9,7 @@ import java.util.Date;
  * <p>
  * Utility class providing methods to convert various Java Date/Time objects
  * into formatted String representations using the modern Date-Time API.
+ * </p>
  *
  * @author Xu
  * @version 1.0
@@ -19,10 +20,13 @@ public class ConvertDateToString {
     public static void main(String[] args) {
         // Create a formatter to convert the current date and time into the pattern yyyy/MM/dd HH:mm:ss
         SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy/MM/dd HH:mm:ss");
+
         // Get the current system date and time
         Date date = new Date();
+
         // Format the Date object into a string
         String dateString = dateFormat.format(date);
+
         // Print the formatted date-time string
         System.out.println(dateString);
     }
